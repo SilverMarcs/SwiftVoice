67ef582efe20a0bd5e44653fc7e3d77de80ae0f1
Zabir Raihan <zabirraihan@icloud.com>
2026-03-16 13:17:28 +0600

    Initial Commit

---
+++ SwiftVoice/SwiftVoiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftVoiceApp.swift
+//  SwiftVoice
+//
+//  Created by Zabir Raihan on 16/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct SwiftVoiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
